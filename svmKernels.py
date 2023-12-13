9c9b238e9ab1022fe04dd0fa24f1cb28c308573c
Minh <minh@gmail.com>
2023-12-14 00:13:17 +0700

    SVM code

--- svmKernels.py
+++ svmKernels.py
@@ -20,7 +20,11 @@ def myPolynomialKernel(X1, X2):
         Returns:
             An n1-by-n2 numpy array representing the Kernel (Gram) matrix
     '''
-    return #TODO
+    global _polyDegree
+
+    dot_product = np.dot(X1, X2.T)
+    polynomialKernelMatrix = (dot_product + 1)** _polyDegree
+    return polynomialKernelMatrix
 
 
 
@@ -32,6 +36,15 @@ def myGaussianKernel(X1, X2):
         Returns:
             An n1-by-n2 numpy array representing the Kernel (Gram) matrix
     '''
+
+    global _gaussSigma
+
+    pairwise_distance = np.sum(X1**2 , axis = 1).reshape(-1,1) + np.sum(X2**2, axis = 1) - 2*np.dot(X1,X2.T)
+    gaussian_kernel_matrix = np.exp(-pairwise_distance/ ( 2 * _gaussSigma ** 2))
+
+    return gaussian_kernel_matrix
+
+
     return #TODO
 
 
